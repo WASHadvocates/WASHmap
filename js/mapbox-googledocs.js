b52708de5c480cf8a32463c72544576b35f2e696
WASHadvocates <wash.event@gmail.com>
2013-06-19 15:34:26 -0300

    Update mapbox-googledocs.js

--- js/mapbox-googledocs.js
+++ js/mapbox-googledocs.js
@@ -1,73 +1,95 @@
-// Takes a spreadsheet key, worksheet ID, and callback function with a
-// features argument. Use this to find the worksheet ID:
-// https://spreadsheets.google.com/feeds/worksheets/[spreadsheet_key]/public/basic?alt=json
+(function(app) {
+    	var m;
+		var layers = document.getElementById('layers');
+		var markers;
+		var features;
 
-if (typeof mapbox === 'undefined') mapbox = {};
-if (typeof mapbox.converters === 'undefined') mapbox.converters = {};
+	   mapbox.auto('map', 'nate.map-mjvvnjm5', getData) 
 
-mapbox.converters.googledocs = function(spreadsheet, sheet, callback) {
-    if (typeof reqwest === 'undefined') {
-        throw 'CSV: reqwest required for mapbox.converters.googledocs';
-    }
+		var interaction = mapbox.interaction();
+			interaction.map(map);
 
-    function response(x) {
-        var features = [],
-            latfield = '',
-            lonfield = '';
-            StateLatfield = '';
-            StateLonfield = '';
-            Organizationfield = '';
-            Projectfield = '';
-            Targetfield = '';
-            Descriptionfield = '';
-            Cityfield = '';
-            Statefield = '';
-        if (!x || !x.feed) return features;
+	    // Get data
+	    function getData(map) {
+			m = map;
+			mmg_google_docs('0Agz3kF-4PjNPdHRqQldhSUNsLVpZYURucE0tUEYxclE', mapData);
+		}
 
-        for (var f in x.feed.entry[0]) {
-            if (f.match(/\$Latitude/i)) latfield = f;
-            if (f.match(/\$Longitude/i)) lonfield = f;
-            if (f.match(/\$StateLat/i)) StateLatfield = f;
-            if (f.match(/\$StateLon/i)) StateLonfield = f;
-            if (f.match(/\$Organization_Name/i)) Organizationfield = f;
-            if (f.match(/\$Project_Name/i)) Projectfield = f;
-            if (f.match(/\$Target_Countries/i)) Targetfield = f;
-            if (f.match(/\$Description/i)) Descriptionfield = f;
-            if (f.match(/\$City/i)) Cityfield = f;
-            if (f.match(/\$State/i)) Statefield = f;
+		// Build map
+		function mapData(f){ 
+			features = f;
+			markers = mapbox.markers.layer()
+				//.factory(factory)    
+				.features(f);            
+			mapbox.markers.interaction(markers);
+			m.addLayer(markers);  
+			newMarker();
+
+		}
+
+		function newMarker() {
+    		if (window.location.hash === '#new') {
+        		$('#new').fadeIn('slow');
+        		window.location.hash = '';
+        		window.setTimeout(function() {
+            		$('#new').fadeOut('slow');
+        		}, 4000)
+    		}
         }
 
-        for (var i = 0; i < x.feed.entry.length; i++) {
-            var entry = x.feed.entry[i];
-            var feature = {
-                geometry: {
-                    type: 'Point',
-                    coordinates: [latfield, lonfield]
-                },
-                properties: {}
-            };
-            for (var y in entry) {
-                if (y === latfield) feature.geometry.coordinates[1] = parseFloat(entry[y].$t);
-                else if (y === lonfield) feature.geometry.coordinates[0] = parseFloat(entry[y].$t);
-                else if (y.indexOf('gsx$') === 0) {
-                    feature.properties[y.replace('gsx$', '')] = entry[y].$t;
-                }
-            }
-            if (feature.geometry.coordinates.length == 2) features.push(feature);
-        }
+		// google_docs.js
+		function mmg_google_docs(id, callback) {
+		    if (typeof reqwest === 'undefined') {
+		        throw 'CSV: reqwest required for mmg_csv_url';
+		    }
 
-        return callback(features);
-    }
+		    function response(x) {
+		        var features = [],
+		            latfield = '',
+		            lonfield = '';
+		        if (!x || !x.feed) return features;
 
-    // Get sheet id with this:
-    var url = 'https://spreadsheets.google.com/feeds/list/' + spreadsheet +
-        '/' + sheet + '/public/values?alt=json-in-script&callback=callback';
+		        for (var f in x.feed.entry[0]) {
+		            if (f.match(/\$lat/i)) latfield = f;
+		            if (f.match(/\$lon/i)) lonfield = f;
+		        }
 
-    reqwest({
-        url: url,
-        type: 'jsonp',
-        jsonpCallback: 'callback',
-        success: response,
-        error: response
-    });
-};
+		        for (var i = 0; i < x.feed.entry.length; i++) {
+		            var entry = x.feed.entry[i];
+		            var feature = {
+		                geometry: {
+		                    type: 'Point',
+		                    coordinates: []
+		                },
+		                properties: {
+							'marker-color':'#F33',
+							'title': 'Last update: ' + entry['gsx$timenow'].$t,
+							'description': entry['gsx$tweet'].$t
+						}
+		            };
+		            for (var y in entry) {
+		                if (y === latfield) feature.geometry.coordinates[1] = parseFloat(entry[y].$t);
+		                else if (y === lonfield) feature.geometry.coordinates[0] = parseFloat(entry[y].$t);
+		                else if (y.indexOf('gsx$') === 0) {
+		                    feature.properties[y.replace('gsx$', '')] = entry[y].$t;
+		                }
+		            }
+		            if (feature.geometry.coordinates.length == 2) features.push(feature);
+		        }
+
+		        return callback(features);
+		    }
+
+		    var url = 'https://spreadsheets.google.com/feeds/list/' + id +
+		        '/4/public/values?alt=json-in-script&callback=callback';
+
+		    reqwest({
+		        url: url,
+		        type: 'jsonp',
+		        jsonpCallback: 'callback',
+		        success: response,
+		        error: response
+		    });
+		}
+
+}({}));    
